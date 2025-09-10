64144afe956f7b2431f33629904d8cf97ef48375
w0nde4 <65129493+w0nde4@users.noreply.github.com>
2025-09-10 21:13:53 +0400

    initial

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+    [SerializeField] State startingState;
+    State state;
+
+    private void Start()
+    {
+        state = startingState;
+        storyText.text = state.GetStateStory();
+    }
+
+    private void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+        }
+
+        storyText.text = state.GetStateStory();
+    }
+}
